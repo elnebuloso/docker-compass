4ff9e6d93eb5d80fabb8bfdb6ef3dfd29ab988ce
Administrator <jan.thoennessen@gmail.com>
2017-08-04 23:16:24 +0200

    ## [1.1.0] - 2017-08-04 ### Added - Added docker-entrypoint.sh

---
+++ public/scss/config.rb
@@ -0,0 +1,11 @@
+require 'compass/import-once/activate'
+
+project_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+http_path   = "/"
+css_dir     = "css"
+sass_dir    = "scss"
+images_dir  = "images"
+
+relative_assets = true
+line_comments   = true
